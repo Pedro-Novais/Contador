c1a6da5153aa43cb4cafb18786ba60b69657ffba
Pedro-Novais <phnovais7@gmail.com>
2024-03-30 00:43:40 -0300

    Correções

--- script.js
+++ script.js
@@ -25,17 +25,17 @@ btn_start.addEventListener("click", verifield)
 
 btn_stop.addEventListener("click", stop)
 
-function maxLengthCheck(object){
+function maxLengthCheck(object) {
     if (object.value.length > object.maxLength);
-      object.value = object.value.slice(0, object.maxLength);
+    object.value = object.value.slice(0, object.maxLength);
 }
 
-function verifield(){
-    if(exec == 0){
+function verifield() {
+    if (exec == 0) {
         exec = 1;
         let input = document.querySelector('#choose-count').value;
         view_num.innerText = input;
-        if(input<0){
+        if (input < 0) {
             let verIf = 0;
             style(div_alert, verIf)
             pop();
@@ -43,7 +43,7 @@ function verifield(){
             view_num.innerText = "";
             return false
         }
-        if(input==""){
+        if (input == "") {
             let verIf = 1;
             style(div_alert, verIf)
             pop();
@@ -51,7 +51,7 @@ function verifield(){
             view_num.innerText = "";
             return false
         }
-        if(i==0){
+        if (i == 0) {
             btn_stop.classList.remove('buttons-state');
             btn_stop.classList.add('buttons');
             btn_start.classList.remove('buttons');
@@ -59,44 +59,45 @@ function verifield(){
 
             let verIf = 2;
             style(div_alert, verIf)
-            i=1;
+            i = 1;
             progressBar(input);
             input--;
-            interval = setInterval(() =>{
-            if(input!=-1){
-                view_num.innerText = input;
-                input--;
-            }
-            if(input==-1){
-                i = 0;
-                exec = 0;
-                div_alert.classList.add('dialog');
+            interval = setInterval(() => {
+                if (input != -1) {
+                    view_num.innerText = input;
+                    input--;
+                }
+                if (input == -1) {
+                    i = 0;
+                    exec = 0;
+                    div_alert.classList.add('dialog');
 
-                btn_stop.classList.remove('buttons');
-                btn_stop.classList.add('buttons-state');
-                btn_start.classList.remove('buttons-state');
-                btn_start.classList.add('buttons');
+                    btn_stop.classList.remove('buttons');
+                    btn_stop.classList.add('buttons-state');
+                    btn_start.classList.remove('buttons-state');
+                    btn_start.classList.add('buttons');
 
-                setTimeout(barBack, 500);
-                clearInterval(interval)
-                clearInterval(identy)
-            }
-        }, 1000);
-    }}
+                    setTimeout(barBack, 500);
+                    clearInterval(interval)
+                    clearInterval(identy)
+                }
+            }, 1000);
+        }
+    }
 }
 
-function style(a,b){
-    if(b==0){
-        a.innerText="Digite algum número maior que zero!";
-    }else if(b==1){
-        a.innerText="Digite algum número para prosseguir!";
-    }else{
+function style(a, b) {
+    if (b == 0) {
+        a.innerText = "Digite algum número maior que zero!";
+    } else if (b == 1) {
+        a.innerText = "Digite algum número para prosseguir!";
+    } else {
         a.classList.remove('dialog');
     }
-    a.style.textTransform='uppercase';
+    a.style.textTransform = 'uppercase';
 }
 
-function stop(){
+function stop() {
     btn_stop.classList.remove('buttons');
     btn_stop.classList.add('buttons-state');
     btn_start.classList.remove('buttons-state');
@@ -110,42 +111,42 @@ function stop(){
     div_alert.classList.add('dialog');
 }
 
-function pop(){
-    $( function() {
-        $( ".dialog" ).dialog({
-          draggable: false,
-          modal: true,
-          title: "ATENÇÃO!",
-          show: {
-            effect: "fade",
-            duration: 500
-          },
-          hide: {
-            effect: "fade",
-            duration: 250
-          }
+function pop() {
+    $(function () {
+        $(".dialog").dialog({
+            draggable: false,
+            modal: true,
+            title: "ATENÇÃO!",
+            show: {
+                effect: "fade",
+                duration: 500
+            },
+            hide: {
+                effect: "fade",
+                duration: 250
+            }
         });
-        $( "#button-start" ).on( "click", function() {
-          $( ".dialog" ).dialog( "open" );
-          $( ".dialog" ).dialog( "destroy" );
+        $("#button-start").on("click", function () {
+            $(".dialog").dialog("open");
+            $(".dialog").dialog("destroy");
         });
-      });
-    }
+    });
+}
 
-function progressBar(time){
+function progressBar(time) {
     let velocity = (time / 100) * 1000;
     width = 100;
     identy = setInterval(scene, velocity);
-    function scene(){
-        if(width <= 0){
+    function scene() {
+        if (width <= 0) {
             clearInterval(identy)
-        }else{
+        } else {
             width--;
             progress.style.width = width + '%';
         }
     }
 }
 
-function barBack(){
-    progress.style.width="100%"
+function barBack() {
+    progress.style.width = "100%"
 }
